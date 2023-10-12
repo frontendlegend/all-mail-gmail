b87d87c60378d5961a43ce0e410f8c3ae64329a6
frontendlegend <aryngazy.khan@gmail.com>
2023-10-12 13:45:27 +0600

    auto redirect to all inboxes, when navigating from new tab to gmail

--- amg-bg.js
+++ amg-bg.js
@@ -1,3 +1,24 @@
-chrome.action.onClicked.addListener(({ url }) => {
-    chrome.tabs.update(undefined, { url: url.replace('#inbox', '#all') });
+const NEW_TAB_URL = 'chrome://new-tab-page/';
+const GMAIL_URL = 'https://mail.google.com/';
+
+const readStorageUrl = async (tabId) => {
+    return new Promise(resolve => {
+        chrome.storage.session.get(tabId, (item) => resolve(item[tabId]));
+    });
+};
+
+chrome.webNavigation.onCommitted.addListener(async (data) => {
+    if (data.frameId !== 0) return; // Don't trigger on iframes
+
+    const tabId = data.tabId.toString();
+    const currentUrl = data.url;
+    const prevUrl = await readStorageUrl(tabId);
+
+    if (!prevUrl && currentUrl == NEW_TAB_URL) 
+        chrome.storage.session.set({ [tabId]: currentUrl });
+
+    if (prevUrl == NEW_TAB_URL && currentUrl.indexOf(GMAIL_URL) != -1) {
+        chrome.tabs.update(+tabId, { url: currentUrl + '#all' });
+        chrome.storage.session.set({});
+    }
 });
